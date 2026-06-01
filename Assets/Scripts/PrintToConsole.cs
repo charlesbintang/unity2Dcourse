06d043a83e0c6bc07f0568fd2a0217be723a609b
charlesbintang <charlesbintang203@gmail.com>
2026-06-01 22:42:55 +0700

    feat: implement printToConsole script (C1:5)

--- Assets/Scripts/PrintToConsole.cs
+++ Assets/Scripts/PrintToConsole.cs
@@ -5,12 +5,12 @@ public class PrintToConsole : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Destroy the enemy!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
