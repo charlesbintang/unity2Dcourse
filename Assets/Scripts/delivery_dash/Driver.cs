73fa031561d401332bf33d7a0801fe13a4cb3c0a
charlesbintang <charlesbintang203@gmail.com>
2026-06-02 13:35:28 +0700

    feat: add transform and translate to the car (2:3 // 2:10)

--- Assets/Scripts/delivery_dash/Driver.cs
+++ Assets/Scripts/delivery_dash/Driver.cs
@@ -5,12 +5,13 @@ public class Driver : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0, 0.1f, 0);
+        transform.Rotate(0, 0, -1.5f);
     }
 }
