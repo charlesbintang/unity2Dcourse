32ad2177ab73eb5ab1aab836702831fac3c58af0
charlesbintang <charlesbintang203@gmail.com>
2026-06-02 18:05:03 +0700

    feat: moving the car with wasd key (2:13)

--- Assets/Scripts/delivery_dash/Driver.cs
+++ Assets/Scripts/delivery_dash/Driver.cs
@@ -1,19 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = -1.5f;
     [SerializeField] float moveSpeed = 0.1f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, moveSpeed, 0);
-        transform.Rotate(0, 0, steerSpeed);
+        float move = 0f;
+        float steer = 0f;
+
+        if (Keyboard.current.wKey.isPressed)
+        {
+            move = 1f;
+        }
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            move = -1f;
+        }
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            steer = -1f;
+        }
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            steer = 1f;
+        }
+
+        transform.Translate(0, moveSpeed * move, 0);
+        transform.Rotate(0, 0, steerSpeed * steer);
     }
 }
