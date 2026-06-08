8dd74fa64620c41562578e17b85e246b3d62da08
charlesbintang <charlesbintang203@gmail.com>
2026-06-08 11:40:59 +0700

    feat: Using OnTriggerEnter2D

--- Assets/Scripts/delivery_dash/Collision.cs
+++ Assets/Scripts/delivery_dash/Collision.cs
@@ -6,4 +6,9 @@ public class Collision : MonoBehaviour
     {
         Debug.Log("Ouch!");
     }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Triggered!");
+    }
 }
